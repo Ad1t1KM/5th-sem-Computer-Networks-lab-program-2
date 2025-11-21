ec40d152fdd0ca55891018fe0ef86dc7250076fe
Aditi Keshavmurthy <aditiK1051@gmail.com>
2025-11-21 21:08:53 +0530

    TCPS order of import statement changed

--- TCP/TCPS.java
+++ TCP/TCPS.java
@@ -1,13 +1,13 @@
 package TCP;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.FileReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.ServerSocket;
 
 public class TCPS {
     public static void main(String[] args) throws Exception {
